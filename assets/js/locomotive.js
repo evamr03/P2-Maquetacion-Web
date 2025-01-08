a92be0a2182cb04ceb801cff99d709a4568dadd1
Airin7 <irenefierro02@gmail.com>
2025-01-08 18:43:49 +0100

    Locomotive-scroll
    No funciona

--- assets/js/locomotive.js
+++ assets/js/locomotive.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const scroll = new LocomotiveScroll({
         el: document.querySelector("[data-scroll-container]"),
-        smooth: true,   
+        smooth: true,
     });
 });
