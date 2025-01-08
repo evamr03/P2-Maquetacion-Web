6718fda21dbf211130fcdcc27344c2c3e8f2d290
Airin7 <irenefierro02@gmail.com>
2025-01-08 23:00:52 +0100

    Locomotive-Scroll

--- assets/js/locomotive.js
+++ assets/js/locomotive.js
@@ -1,4 +1,10 @@
 /*
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("container-scroll"),
+  smooth:true,
+  lerp: 0.1
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     const scroll = new LocomotiveScroll({
       el: document.querySelector("[data-scroll-container]"),
@@ -9,4 +15,22 @@ document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener('load', () =>{
     scroll.update();
 })
-    */
+*/
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("scroll-container"),
+  smooth:true,
+  lerp: 0.1
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+      lerp: 0.1
+  });
+
+  window.addEventListener('load', () => {
+      scroll.update();
+  });
+});
